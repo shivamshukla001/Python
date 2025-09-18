25b856a14882b81bff3b29cce255cb75553beafa
Shivam.tech <shivamshukla6350@gmail.com>
2025-09-18 10:28:10 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,31 @@
+# String
+first_name="Shivam"
+food="Pizza"
+destination="Goa"
+# FORMAT METHOD 1
+# print( f"My name is {first_name} I love {food} and I want to visit {destination}" )
+
+# print( "My name is "+ first_name + " I love " + food + " and I want to visit " + destination )
+
+# Integer
+age=21
+year=2024
+height=5.9
+# print(f"my age is {age} and I was born in {year} and my height is {height}")
+
+# Float
+cgpa=8.7
+Math=77.9
+english=89.3
+# print(f"My cgpa is {cgpa} and I got {Math} in math and {english} in english")
+
+# Boolean
+
+isStudent=False
+notStudent=True
+if isStudent:
+    print("I am a student")
+elif notStudent:
+    print("I am a Employee")
+else:
+    print("I am not a Student")
