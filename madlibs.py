c57db3eddb06a7cd1584c7c41c54fa942779a1b1
Shivam.tech <shivamshukla6350@gmail.com>
2025-09-18 18:12:39 +0530

    game

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+animal = input("Enter an animal: ")
+place = input("Enter a place: ")
+food = input("Enter a type of food: ")
+funny_noise = input("Enter a funny noise: ")
+verb = input("Enter a verb: ")
+
+print(f"Today, I went to the zoo with my (noun). First, we saw a {adjective} {animal} jumping up and down in its {place}. Then, we bought a {adjective}{food} to eat. My friend dropped theirs on the {noun} and it made a {funny_noise}. Later, we watched a {adjective} {animal} try to {verb}, which made everyone {verb}. It was the most {adjective} day ever!")
