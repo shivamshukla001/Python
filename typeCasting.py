66d4b57db94a8a9d8df99ce939511ded691bc753
Shivam.tech <shivamshukla6350@gmail.com>
2025-09-18 10:41:37 +0530

    type casting

---
+++ typeCasting.py
@@ -0,0 +1,13 @@
+# The process of converting one data type to another is called type casting.
+
+name="shivam"
+age=21
+height=5.9
+is_adult=True
+
+print(type(height))
+
+floats = int(height)
+
+print(type(floats))
+print(floats)
