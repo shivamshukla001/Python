bf677b8e78fe6b9e8978c1c2999a8e3beb0f1009
Shivam.tech <shivamshukla6350@gmail.com>
2025-09-18 19:14:43 +0530

    if-statement

---
+++ ifStatement.py
@@ -0,0 +1,18 @@
+# response = input("Enter a number: ")
+
+# if response == "1":
+#     print("You entered one.")
+# elif response == "2":
+#     print("You entered two.")
+# elif response == "3":
+#     print("You entered three.")
+# else:
+#     print("You entered a number other than one, two, or three.")
+
+# Booleans use in if-else statements
+for_sale=False
+
+if for_sale:
+    print("This item is for sale.")
+else:
+    print("this item is not for sale")
