dd06774d762d861539a40c64aaaf75400d8b0b27
Shivam.tech <shivamshukla6350@gmail.com>
2025-09-18 11:14:04 +0530

    inputs in python

---
+++ input.py
@@ -0,0 +1,25 @@
+# input= input is a function in Python that allows you to take input from the user.
+# The input function reads a line from input, converts it into a string (stripping any trailing newline), and returns that. 
+# The syntax of the input function is: input(prompt)
+
+
+# name= input("Ener your name:")
+# print(f"helllo {name}")
+
+# Exercide 1: print the area of a rectangle
+
+# length= int(input("Enter the length of rectangle:"))
+# width = int(input("Enter the width of rectangle:"))
+# area = length*width
+# print(f"the are of rectangle is {area}cm²")
+
+
+# Numlock + alt + 0178 = ²
+
+# Exercise 2: shoping cart problem
+
+name= input("enter the name of item you would like to buy")
+price = float(input("enter the price of that item"))
+quantity= int(input("Enter the quantity of that item"))
+total = price*quantity
+print(f"the total cost of {quantity} {name} is ${total}")
