0bf662cdac9fedccf40351115ca43b6135483a6a
Shivam.tech <shivamshukla6350@gmail.com>
2025-09-19 18:26:33 +0530

    indexing in string

---
+++ indexing.py
@@ -0,0 +1,11 @@
+credit_number = "1234-5678-9101-1121"
+# [start:end:step]
+# This is a sample code snippet for indexing functionality.
+
+print(credit_number[0])
+print(credit_number[0:4])
+print(credit_number[:4])
+print(credit_number[5:9])
+print(credit_number[5:])
+print(credit_number[-3])
+print(credit_number[:: 2])
