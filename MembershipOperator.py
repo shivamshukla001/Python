ca6c1fbc85d7fa7cc92f664914e13f10a2a08c8d
Shivam.tech <shivamshukla6350@gmail.com>
2025-10-18 17:46:33 +0530

    membershop operatore in python

---
+++ MembershipOperator.py
@@ -0,0 +1,55 @@
+# Membership operator = in, not in
+# Used to test if a sequence is presented in an object like a string, list, tuple, set, or dictionary
+
+word  =  "Hello, welcome to the world of Python programming"
+
+# letter = input("Enter a letter to check if it is in the word: ")
+
+# if letter in word:
+#     print(f"The letter '{letter}' is present in the word.")
+# else:
+#     print(f"The letter '{letter}' is not present in the word.")
+
+# if letter not in word:
+#     print(f"The letter '{letter}' is not present in the word.")
+# else:
+#     print(f"The letter '{letter}' is present in the word.")
+
+
+# set_of_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}    
+# number = int(input("Enter a number to check if it is in the set of numbers: "))
+
+# if number in set_of_numbers:
+#     print(f"The number {number} is present in the set of numbers.")
+# else:
+#     print(f"The number {number} is not present in the set of numbers.")
+
+
+# student= {"shivam", "kumar", "anand", "rahul"}
+# find_student = input( "Enter the name of a student you want to seach: ")
+
+# if find_student in student:
+#     print(f"The student '{find_student}' is present in the student list.")
+# else:
+#     print(f"The student '{find_student}' is not present in the student list.")
+
+# grades = {
+#     "Alice": 85,
+#     "Bob": 92,
+#     "Charlie": 78,
+#     "David": 90
+# }
+
+# student = input("Enter the name of the student to check their grade: " )
+
+# if student in grades:
+#     print(f"{student}'s grade is: {grades[student]}")
+# else:
+#     print(f"{student} is not found in the grades dictionary.")
+
+# email = input("Enter your email address: ")
+
+# if "@" in email and "." in email and "com" in email:
+#     print("Valid email address.")
+# else:
+#     print("Invalid email address.")
