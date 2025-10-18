9fdb3d0c287cc65612228d477b515086e72c8ac7
Shivam.tech <shivamshukla6350@gmail.com>
2025-10-18 17:46:03 +0530

    random in Python

---
+++ Random.py
@@ -0,0 +1,25 @@
+import random
+
+# print(help(random))
+
+# NUMBER = random.randint(1, 100)
+# print(f"Random number between 1 and 100: {NUMBER}")
+
+# low = 1
+# high = 10
+# number = random.randint(low, high)
+# print(f"Random number between {low} and {high}: {number}")
+
+options = ("rock", "paper", "scissors")
+
+# choice = random.choice(options)
+# print(f"Random choice from options {options}: {choice}")
+
+cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+# random.shuffle(cards)
+# print(f"Shuffled cards: {cards}")
+
+
+
+
+
